6d6ea3a18dca49effebf00d0310edc53999367a6
Kenny <106347478+kennyhml@users.noreply.github.com>
2023-07-30 20:28:35 +0200

    improved tree rendering lots

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -1,6 +1,6 @@
 var canvas = document.querySelector('canvas');
 
-canvas.width = window.innerWidth - 2;
+canvas.width = window.innerWidth - 322;
 canvas.height = window.innerHeight - 10;
 
 var context = canvas.getContext("2d")
@@ -8,8 +8,8 @@ var context = canvas.getContext("2d")
 import {BinaryTree} from "./tree/binary_tree.js"
 import {TreeNode} from "./tree/tree_node.js"
 
-console.log(canvas.width - canvas.offsetLeft)
-const rootNode = new TreeNode(1, (canvas.width - canvas.offsetLeft) / 2, 150);
+
+const rootNode = new TreeNode(1, (canvas.width) / 2, 150);
 rootNode.left = new TreeNode(2, -50, 40);
 rootNode.right = new TreeNode(3, 50, 40);
 
@@ -17,9 +17,11 @@ rootNode.right = new TreeNode(3, 50, 40);
 rootNode.left.left = new TreeNode(4, -80, 80);
 rootNode.left.right = new TreeNode(5, -20, 80);
 rootNode.right.left = new TreeNode(6, 20, 80);
+rootNode.right.right = new TreeNode(6, 20, 80);
 
-const tree = new BinaryTree(rootNode);
-tree.draw(canvas, context)
+
+const tree = new BinaryTree(canvas, context, rootNode);
+tree.draw()
 
 
 
--- static/js/tree/binary_tree.js
+++ static/js/tree/binary_tree.js
@@ -4,6 +4,8 @@ export { BinaryTree }
 
 
 const scaleFactor = 0.075;
+const verticalSpacing = 50;
+const padding = 50;
 
 /**
  * Represents a binary tree on the canvas.
@@ -25,8 +27,12 @@ class BinaryTree {
      * If no root node is given, the first inserted node will be
      * the root node.
      * @param {TreeNode} root - The root node of the binary tree 
+     * @param {HTMLCanvasElement} canvas - The canvas we are drawing on
+     * @param {CanvasRenderingContext2D} context - The context object to draw with
      */
-    constructor(root = null) {
+    constructor(canvas, context, root = null) {
+        this.canvas = canvas;
+        this.context = context;
         this.root = root;
         this.selectedNode = root;
         this.radius = 0;
@@ -51,33 +57,22 @@ class BinaryTree {
         };
         this.selectedNode = newNode;
     };
+    /**
+     * Calculates the radius of each node based on the size of the canvas
+     * and the size of the binary tree.
+     */
+    calculateNodeRadius() {
+        let widthFactor = this.canvas.width / this.width
+        let heightFactor = this.canvas.height / this.height
 
-    calculateRadius(canvas) {
-        var factorWidth = (canvas.width - canvas.offsetLeft) / this.width;
-        var factorHeight = canvas.height / this.height;
-        this.radius = Math.min(factorWidth, factorHeight) * scaleFactor;
+        this.radius = Math.min(widthFactor, heightFactor) * scaleFactor;
     };
     /**
      * @return {number} The maximum width of the binary tree at any level.
      */
     get width() {
-        var level = [this.root];
-        var maxWidth = 0
-
-        while (this.root && level.length > 0) {
-            maxWidth = Math.max(maxWidth, level.length);
-            let nextLevel = [];
-
-            level.forEach(node => {
-                if (node.left) {
-                    nextLevel.push(node.left);
-                }
-                if (node.right) {
-                    nextLevel.push(node.right);
-                };
-            });
-            level = nextLevel;
-        };
+        const levels = this.toArray();
+        const maxWidth = Math.max(...levels.map(nodes => nodes.length));
         return maxWidth;
     };
     /**
@@ -98,45 +93,115 @@ class BinaryTree {
         dfs(this.root, maxHeight);
         return maxHeight;
     };
+    /**
+     * Returns a level order traversal of the binary tree as array.
+     * @returns {Array} - The levels of the tree where each level contains its nodes.
+     */
+    toArray() {
+        var levels = [];
+        var level = [this.root];
 
-    draw(canvas, context) {
-        if (!this.root) { return; };
-        this.calculateRadius(canvas);
-        this.drawNode(this.root, context)
-        this.drawChildren(this.root, context);
-    }
+        while (this.root && level.length > 0) {
+            let nextLevel = [];
+            level.forEach(node => {
+                if (node.left) {
+                    nextLevel.push(node.left);
+                }
+                if (node.right) {
+                    nextLevel.push(node.right);
+                };
+            })
+            levels.push(level)
+            level = nextLevel;
 
-    calculateChildPosition(node, side) {
-        
-        if (!["left", "right"].includes(side)) {
-            throw TypeError(`Unknown child '${side}'.`)
-        }
-
-        var x = node.x + (side == 'right' ? 50 : -50)
-        var y = node.y + 50
-
-        return [x, y];
+        };
+        return levels;
     };
 
-    drawNode(node, context) {
-        console.log(`Drawing ${node.x} ${node.y}`)
-        context.beginPath();
-        context.arc(node.x, node.y, this.radius, 0, Math.PI * 2);
-        context.stroke();
-
-    }
-    drawChildren(node, context) {
-        if (node.left) {
-            node.left.setPos(...this.calculateChildPosition(node, "left"));
-            this.drawNode(node.left, context);
-            this.drawChildren(node.left, context);
+    draw() {
+        if (!this.root) {
+            console.log("Tree was not drawn as it has no nodes.")
+            return;
         };
 
-        if (node.right) {
-            node.right.setPos(...this.calculateChildPosition(node, "right"));
-            this.drawNode(node.right, context);
-            this.drawChildren(node.right, context);
-        };
+        // Calculate the node radius based on the canvas size and the
+        // max width or height of the binary tree.
+        this.calculateNodeRadius();
+        console.log(`Calculated circle for radius: ${this.radius}`);
 
-    }
+        // Reverse the array representation of the tree to start building it
+        // bottom - up.
+        var levels = this.toArray();
+        levels.reverse()
+        console.log("Levels:", levels)
+
+        this.drawNodes(levels);
+    };
+    /**
+     * Draws a singular node on the canvas, granted the nodes position
+     * has been set beforehand.
+     * @param {TreeNode} node - The node to draw on the canvas.
+     */
+    drawNode(node) {
+        console.log(`Drawing node ${node.value} at ${node.x} ${node.y}`)
+        this.context.beginPath();
+        this.context.arc(node.x, node.y, this.radius, 0, Math.PI * 2);
+        this.context.stroke();
+    };
+    connectNode(node) {
+        console.log(`Connecting node ${node.value} to it's children.`)
+        for (let child of [node.left, node.right]) {
+            if (child == null) { return; }
+
+            const angleToChild = Math.atan2(child.y - node.y, child.x - node.x);
+            const xOffset = Math.cos(angleToChild) * this.radius;
+            const yOffset = Math.sin(angleToChild) * this.radius;
+
+            this.context.beginPath();
+            this.context.moveTo(node.x + xOffset, node.y + yOffset);
+            this.context.lineTo(child.x - xOffset, child.y - yOffset);
+            this.context.stroke();
+        };
+    };
+    /**
+     * Draws the nodes of the binary tree, starting from the bottom most level
+     * to avoid node collisions.
+     * @param {Array} levels - The levels of the binary tree in reverse
+     */
+    drawNodes(levels) {
+        let nodes = levels[0].length;
+        console.log(`Constructing lowermost level with ${nodes} nodes..`);
+
+        // Compute the spacings and centers so we can space the nodes properly
+        var center = (this.canvas.width) / 2;
+        var horizontalSpacing = (this.canvas.width - padding * 2) / nodes;
+        var verticalSpacing = (this.canvas.height - padding * 2) / nodes;
+
+        console.log(`Horizontal space: ${horizontalSpacing}, vertical space: ${verticalSpacing}.`);
+
+        // We know how much space we need per level (verticalSpacing), and we know
+        // how many levels we have, so we can calculate the y-position of the final level that way
+        var startY = (verticalSpacing * levels.length) + padding;
+        // We know that the middle node of our bottom most level must be at the center
+        var startX = center - (horizontalSpacing * (nodes / 2));
+
+        for (let i = 0; i < levels.length; i++) {
+            let y = startY - verticalSpacing * i;
+            for (let j = 0; j < levels[i].length; j++) {
+                let node = levels[i][j];
+                // Drawing our initial bottom line
+                if (i == 0) {
+                    let x = startX + horizontalSpacing * j;
+                    node.setPos(x, y);
+                    this.drawNode(node);
+                } else {
+                    // Compute position based on children we just placed
+                    let x = (node.left.x + node.right.x) / 2;
+                    node.setPos(x, y);
+                    this.drawNode(node);
+                    this.connectNode(node);
+                };
+            };
+        };
+    };
 };
